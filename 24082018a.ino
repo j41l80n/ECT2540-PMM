138ceca9d12cfb8fc3aef51f6af33159892821d8
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-08-24 10:04:30 -0300

    24082018a

---
+++ 24082018a.ino
@@ -0,0 +1,25 @@
+int quantidade;
+int estado;
+
+void setup() {
+  Serial.begin(9600);
+  quantidade = 0;
+  pinMode(A0, INPUT_PULLUP);
+  estado = 1;
+}
+
+void loop() {
+  // estado 1
+  if (estado == 1 && !digitalRead(A0)) {
+    quantidade++;
+    Serial.println(quantidade);
+    delay(10);
+    estado = 2;
+  }
+
+  // estado 2
+  if (estado == 2 && digitalRead(A0)) {
+    estado = 1;
+    delay(10);
+  }
+}
