4bee48622ca74b883678f05e9b28768709da9d99
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-08-31 10:01:05 -0300

    melhorando maquinas de estado

--- 31082018.ino
+++ 31082018.ino
@@ -1,28 +1,47 @@
 #define LED 13
 #define BOTAO A0
 
-int quantidade = 0;
 int estado = 1;
-int incomingByte = 0;
+int tempoAtual = 0;
+int tempoInicial = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(BOTAO, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
-  
 }
 
 void loop() {
+  tempoAtual = millis();
+  
   // estado 1
   if (estado == 1 && !digitalRead(A0)) {
-    digitalWrite(13, HIGH);
-    delay(3000);
-    digitalWrite(13, LOW);
+    digitalWrite(LED, HIGH);
     estado = 2;
+    tempoInicial = millis();
+    delay(50);
   }
 
+  if(tempoAtual-tempoInicial >= 3000){
+    digitalWrite(LED, LOW);
+    estado = 1;
+  }
+ 
   // estado 2
-  if (estado == 2 && digitalRead(A0)) {
+  if (estado == 2 && digitalRead(BOTAO)) {
+    estado = 3;
+    delay(50);
+  }
+  
+  // estado 3
+  if (estado == 3 && !digitalRead(BOTAO) && digitalRead(LED)) {
+    digitalWrite(13, LOW);
+    estado = 4;
+    delay(50);
+  }
+
+  // estado 4
+  if (estado == 4 && digitalRead(A0)) {
     estado = 1;
     delay(50);
   }
