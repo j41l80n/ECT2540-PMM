883a185bfc5ad3152bca20500d14fdebb46fa9f2
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-11-23 10:58:16 -0300

    finalizando

--- miniprojeto7-cliente/miniprojeto7-cliente.ino
+++ miniprojeto7-cliente/miniprojeto7-cliente.ino
@@ -2,7 +2,7 @@
 #include <Ethernet.h>
 
 #define LED 6
-
+int potenc = 1;
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xAD };
 // endereço do servidor
 char server[] = "10.6.9.81";
@@ -22,14 +22,20 @@ void setup() {
 }
 
 void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
+  bot();
+  delay(1000);
+//  client.println("Connection: close");
+//  client.stop();
+  potenciometro();
+  delay(1000);
+}
+
+void potenciometro() {
   int len = client.available();
   //Serial.println(len);
   if (len > 0) {
     Serial.print("recebendo ");
     Serial.println(len);
-    analogWrite(LED, len);
     byte buffer[80];
     if (len > 80) len = 80;
     client.read(buffer, len);
@@ -37,6 +43,8 @@ void loop() {
     Serial.println();
     String s = String((char *)buffer);
     int v = s.toInt();
+    potenc = v;
+    analogWrite(LED, v);
     Serial.println(v);
   }
 
@@ -55,6 +63,56 @@ void loop() {
       Serial.println(client.remoteIP());
       // Make a HTTP request:
       client.println("GET /?val=pot");
+      client.println("Host: 10.6.9.81");
+      client.println("Connection: close");
+      client.println();
+    } else {
+      Serial.println("connection failed");
+    }
+  }
+}
+
+void bot() {
+  int len = client.available();
+  Serial.print("len ");
+  Serial.println(len);
+  if (len > 0) {
+    Serial.print("recebendo ");
+    Serial.println(len);
+    byte buffer[80];
+    if (len > 80) len = 80;
+    client.read(buffer, len);
+    Serial.write(buffer, len); // show in the serial monitor (slows some boards)
+    Serial.println();
+    String s = String((char *)buffer);
+    int v = s.toInt();
+    Serial.print("botao: ");
+    Serial.println(v);
+    if (v == 1)
+    {
+      analogWrite(LED, potenciometro);
+    }
+    else
+    {
+      analogWrite(LED, LOW);
+    }
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    client.stop();
+    Serial.println("disconected");
+    delay(500);
+    Serial.print("connecting to ");
+    Serial.print(server);
+    Serial.println("...");
+
+    // if you get a connection, report back via serial:
+    if (client.connect(server, 80)) {
+      Serial.print("connected to ");
+      Serial.println(client.remoteIP());
+      // Make a HTTP request:
+      client.println("GET /?val=bot");
       client.println("Host: 10.6.9.81");
       client.println("Connection: close");
       client.println();
